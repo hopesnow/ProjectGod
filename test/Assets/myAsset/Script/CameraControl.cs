a9f64d0b804f9dca9607a226f302ff7cbe94d35c
hopesnow <1201637@st.asojuku.ac.jp>
2015-04-15 11:52:50 +0900

    animatorつくりはじめ

---
+++ test/Assets/myAsset/Script/CameraControl.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraControl : MonoBehaviour {
+
+	public Transform player;
+	public float dist;
+
+	// Use this for initialization
+	void Start () {
+		dist = 6;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		transform.position = new Vector3 (player.position.x, player.position.y + dist * (Mathf.Sqrt(3) / 2), player.position.z - dist / 2);
+
+		if (Input.GetMouseButtonDown (0)) {
+
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit = new RaycastHit();
+
+			if(Physics.Raycast(ray, out hit)){
+
+				if(hit.collider.gameObject.CompareTag("field")){
+
+					player.gameObject.SendMessage("MoveTo", hit.point);
+
+				}
+
+			}
+
+		}
+
+	}
+}
---
+++ test/Assets/myAsset/Script/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void MoveTo(Vector3 pos){
+
+		transform.position = new Vector3 (pos.x, 0, pos.z);
+
+	}
+
+}
