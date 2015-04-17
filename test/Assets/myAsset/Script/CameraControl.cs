a83c130335296a640ffcf31adc9dbf3f69fae972
hopesnow <1201637@st.asojuku.ac.jp>
2015-04-17 15:20:45 +0900

    とりあえず移動できるかな？ カメラ移動も

--- test/Assets/myAsset/Script/CameraControl.cs
+++ test/Assets/myAsset/Script/CameraControl.cs
@@ -11,19 +11,25 @@ public class CameraControl : MonoBehaviour {
 
 	public GameObject tapPoint;
 
+	Vector3 movePos;
+
 	// Use this for initialization
 	void Start () {
-		dist = 8;
-		camSpeed = 10.0f;
+		dist = 10;
+		camSpeed = 15.0f;
+
+		transform.position = GetPlayerCamera();
+
+		movePos = new Vector3 ();
 
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-		transform.position = Vector3.Lerp (transform.position, new Vector3 (player.position.x, player.position.y + dist * (Mathf.Sqrt (3) / 2), player.position.z - dist / 2), Time.deltaTime * camSpeed);
 
-		if (Input.GetMouseButtonDown (0)) {
+		//palyer move
+		if (Input.GetMouseButtonDown (1)) {
 
 			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 			RaycastHit hit = new RaycastHit();
@@ -43,5 +49,39 @@ public class CameraControl : MonoBehaviour {
 
 		}
 
+		//Camera Moving
+		if (Input.GetMouseButton (0)) {
+
+			//Moving Map Mode LoL
+//			if (Input.GetMouseButtonDown (0)) {
+//				movePos = Input.mousePosition;
+//
+//			}
+//
+//			transform.position = transform.position + new Vector3(Input.mousePosition.x - movePos.x, 0, Input.mousePosition.y - movePos.y) * 0.01f;
+
+			//Moving Map Mode VG
+			if(Input.GetMouseButtonDown(0)){
+
+			}else{
+				transform.position = transform.position - new Vector3(Input.mousePosition.x - movePos.x, 0, Input.mousePosition.y - movePos.y) * 0.03f;
+			}
+			movePos = Input.mousePosition;
+			
+
+		}
+
+		//Camera Move to Player
+		if (Input.GetKey(KeyCode.Space)) {
+			transform.position = Vector3.Lerp (transform.position, GetPlayerCamera(), Time.deltaTime * camSpeed);
+		} else {
+			
+		}
+
+	}
+
+	Vector3 GetPlayerCamera(){
+		Vector3 pos = new Vector3 (player.position.x, player.position.y + dist * (Mathf.Sqrt (3) / 2), player.position.z - dist / 2);
+		return pos;
 	}
 }
