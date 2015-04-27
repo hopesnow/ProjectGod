0da6ccada861be9c1253ac9ed180fe88011dfc9a
hopesnow <1201637@st.asojuku.ac.jp>
2015-04-27 17:00:39 +0900

    roomstateから

--- test/Assets/myAsset/Script/MyPhotonClient.cs
+++ test/Assets/myAsset/Script/MyPhotonClient.cs
@@ -1,9 +1,28 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+using HashTable = ExitGames.Client.Photon.Hashtable;
+
+
+
+
+public enum PlayerState{
+	room = 0,
+	init = 1,
+	play = 2,
+
+}
+
+public enum RoomState
+{
+    wait = 0,
+    play = 1,
+
+}
+
 
 public class MyPhotonClient : Photon.MonoBehaviour {
-	private PhotonView myPhotonView;
+	public PhotonView myPhotonView = null;
 	public GameObject[] roomUI;
 	public GameObject[] roomName;
 	public GameObject[] roomMember;
@@ -11,6 +30,10 @@ public class MyPhotonClient : Photon.MonoBehaviour {
 	public GameObject createButton;
 	public GameObject myRoom;
 	public GameObject playerName;
+	public GameObject startButton;//ownerのみOnにする
+
+
+
 
 	// Use this for initialization
 	void Start () {
@@ -38,18 +61,25 @@ public class MyPhotonClient : Photon.MonoBehaviour {
 	void OnJoinedRoom(){
 		Debug.Log ("Join Room Sccess : " + PhotonNetwork.room.name);
 
+		myPhotonView = this.GetComponent<PhotonView> ();
+
+		HashTable h = new HashTable (){{"PS", PlayerState.room}};
+		PhotonNetwork.player.SetCustomProperties (h);
+
 		for (int i = 0; i < roomUI.Length; i++) {
 			roomUI[i].SetActive(false);
 		}
 		myRoom.SetActive (true);
 		myRoom.SendMessage ("SetRoomInfo", PhotonNetwork.room);
 		myRoom.SendMessage ("OpenRoom");
-		Debug.Log(PhotonNetwork.playerName);
+		Debug.Log("playerName : " + PhotonNetwork.playerName);
 
 	}
 
 	void OnLeftRoom(){
 		Debug.Log ("Leave Room");
+
+		myPhotonView = null;
 
 		for (int i = 0; i < roomUI.Length; i++) {
 			roomUI[i].SetActive(true);
@@ -93,11 +123,39 @@ public class MyPhotonClient : Photon.MonoBehaviour {
 	void CreateRoom(){
 		Debug.Log ("CreateRoom : " + theRoomName.GetComponent<InputField>().text);
 		PhotonNetwork.CreateRoom (theRoomName.GetComponent<InputField>().text, true, true, 6);
+		startButton.GetComponent<Button> ().interactable = true;
+
+        HashTable h = new HashTable() {{"RS", RoomState.wait}};
+        
 
 	}
 
 	public void JoinRoom(string roomName){
 		PhotonNetwork.JoinRoom (roomName);
+
+
+	}
+
+	public void StartGame(){
+		Debug.Log ("GameMain へ移行");
+		//Send RPC other players
+
+		PhotonNetwork.room.open = false;
+
+//		PhotonNetwork.DestroyAll ();
+
+		if (myPhotonView != null) {
+			myPhotonView.RPC ("ToGameMain", PhotonTargets.All);
+			Debug.Log("send success");
+		}
+
+	}
+
+	[RPC]
+	void ToGameMain(){
+		PhotonNetwork.isMessageQueueRunning = false;
+		Debug.Log ("to game main");
+		Application.LoadLevel ("GameMain");
 
 	}
 
